8a10c92c5e224693b3d3b79b2fb2e26350bd21c5
Dharmin-23 <dharmin23072002@gmail.com>
2024-08-12 14:42:34 +0530

    Added wishlist icon

--- src/Home/Card/Card.js
+++ src/Home/Card/Card.js
@@ -3,17 +3,24 @@ import star from "../../assets/star.png"
 import "./Card.css"
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'; // Hollow Heart Icon
 import FavoriteIcon from '@mui/icons-material/Favorite'; // Filled Heart Icon
+import TryItNowCard from './TryItNowCard';
 
 export default function Card(props) {
     return (
         <div class="max-w-sm bg-white rounded-lg pr-6">
             <div className="wishlist-icon">
-        {props.isInWishlist ? (
-          <FavoriteIcon className="filled-heart-icon" />
-        ) : (
-          <FavoriteBorderIcon className="hollow-heart-icon" />
-        )}
-      </div>
+                {props.isInWishlist ? (
+                    <>
+                        <FavoriteIcon className="filled-heart-icon" />
+                        <div className="tooltip">Remove from Wishlist</div>
+                    </>
+                ) : (
+                    <>
+                        <FavoriteBorderIcon className="hollow-heart-icon" />
+                        <div className="tooltip">Add to Wishlist</div>
+                    </>
+                )}
+            </div>
             <a href="#">
                 <img class="rounded-t-lg" src={props.image} alt="" style={{ height: "350px", width: "100%" }} />
             </a>
@@ -25,7 +32,7 @@ export default function Card(props) {
                         <span className='text-black-700' style={{ fontSize: "1rem", verticalAlign: "0.65ex", fontWeight: 550 }}>{props.decimal}</span>
                     </div>
                     <div className='mt-1 w-fit gap-1 bg-clip-text text-xs opacity-100'>
-                        <button className='flex'>
+                        <button className='flex' onClick={props.onTryItNow}>
                             <div className='h-6 w-6' style={{ display: "flex", alignItems: "flex-end" }}>
                                 <img src={star} style={{ width: "28px", height: "25px" }} />
                             </div>
